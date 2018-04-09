d648638cd18d64076df5171817c4aa568c5ac92b
lights <lights@tutanota.de>
2018-04-09 10:46:52 -0400

    add a basic translation test

---
+++ tests/translate_test.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+from pathlib import Path
+from heybrochecklog.translate import translate_log
+
+LOGS = [
+    ('french-big-calm.log'),
+]
+
+
+@pytest.mark.parametrize(
+    'filename', LOGS)
+def test_translation(filename):
+    log_path = os.path.join(os.path.dirname(__file__), 'logs', 'translations', filename)
+    log_file = Path(log_path)
+    log = translate_log(log_file)
+
+    translated_path = os.path.join(
+        os.path.dirname(__file__), 'logs', 'translations', '{}.translated'.format(filename))
+    with open(translated_path, 'r') as translated_file:
+        translated_contents = translated_file.read()
+
+    assert log['log'] == translated_contents
